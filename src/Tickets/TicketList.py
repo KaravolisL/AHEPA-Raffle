5c9e3385a5924b9902524286d6eba22141788fb9
KaravolisL <lukejrkaravolis@gmail.com>
2019-11-07 21:52:56 -0500

    Initial work on lineEdits

--- src/Tickets/TicketList.py
+++ src/Tickets/TicketList.py
@@ -40,12 +40,12 @@ class TicketList():
 
     def hasRaffleStarted(self):
         """
-        Convenience method to determine whether ticketList is empty or not 
+        Convenience method to determine whether ticketList is empty or not
         :returns: Whether any tickets have been drawn
         :rtype: bool
         """
         return self.numOfTicketsDrawn != 0
-    
+
     def getLastTicketDrawn(self):
         """
         Iterates the list and compares each ticket's numberDrawn field
@@ -60,7 +60,7 @@ class TicketList():
             return None
 
     def removeTicket(self, ticketNumber):
-        """ 
+        """
         Sets the given tickets numberDrawn field and increments numOfTicketsDrawn
         :param int ticketNumber: Number of ticket to remove
         """
@@ -109,3 +109,13 @@ class TicketList():
             if ticket.isDrawn():
                 drawnTickets.append(ticket)
         return drawnTickets
+
+    def getTicket(self, number):
+        """
+        Gets a ticket from the ticketList given a number
+        :param int number: number of ticket
+        :returns: ticket with given number
+        :rtype: Ticket
+        """
+        assert(number > 0 and number < 226)
+        return self.ticketList[number - 1]
--- src/Windows/EditTicketWindow.py
+++ src/Windows/EditTicketWindow.py
@@ -5,12 +5,16 @@ from PyQt5.QtMultimedia import *
 from PyQt5.QtMultimediaWidgets import *
 
 from Windows.WindowBase import WindowBase
+from Tickets.TicketList import TicketList
 
 class EditTicketWindow(WindowBase):
     def __init__(self):
         super().__init__()
+        self.setWindowTitle('Edit Ticket')
         self.makeLayout()
         self.setWindowFlags(Qt.WindowCloseButtonHint)
+
+        self.initLineEdits()
 
 
     def setSize(self):
@@ -27,32 +31,43 @@ class EditTicketWindow(WindowBase):
         """
         Creates labels, fields, and two buttons
         """
-        self.confirmButton = QPushButton('Change Name')
-        # self.confirmButton.clicked.connect()
-
-        self.cancelButton = QPushButton('Cancel')
-        self.cancelButton.clicked.connect(self.cancelEvent)
-
         self.ticketNumberLabel = QLabel('Ticket Number: ')
         self.ticketNumberLabel.setAlignment(Qt.AlignCenter)
         # self.ticketNumberLabel.setFont()
-
         self.ticketNumberLineEdit = QLineEdit()
+
+        self.currentNameLabel = QLabel('Current Name: ')
 
         self.ticketNameLabel = QLabel('Ticket Name: ')
         self.ticketNameLabel.setAlignment(Qt.AlignCenter)
         # self.ticketNameLabel.setFont()
-
         self.ticketNameLineEdit = QLineEdit()
+
+        self.confirmButton = QPushButton('Change Name')
+        # self.confirmButton.clicked.connect()
+        self.cancelButton = QPushButton('Cancel')
+        self.cancelButton.clicked.connect(self.cancelEvent)
 
         self.layout.addWidget(self.ticketNumberLabel, 0, 0)
         self.layout.addWidget(self.ticketNumberLineEdit, 0, 1)
-        self.layout.addWidget(self.ticketNameLabel, 1, 0)
-        self.layout.addWidget(self.ticketNameLineEdit, 1, 1)
-        self.layout.addWidget(self.confirmButton, 2, 0)
-        self.layout.addWidget(self.cancelButton, 2, 1)
+        self.layout.addWidget(self.currentNameLabel, 1, 0, 2, 1)
+        self.layout.addWidget(self.ticketNameLabel, 2, 0)
+        self.layout.addWidget(self.ticketNameLineEdit, 2, 1)
+        self.layout.addWidget(self.confirmButton, 3, 0)
+        self.layout.addWidget(self.cancelButton, 3, 1)
 
+    def initLineEdits(self):
+        """
+        Initialize line edit events and validation
+        """
+        self.ticketNumberLineEdit.textEdited.connect(self.ticketNumberEntered)
+        self.ticketNumberLineEdit.setMaxLength(3)
     
+    def ticketNumberEntered(self):
+        numberEntered = int(self.ticketNumberLineEdit.text())
+        print(numberEntered)
+        self.ticketNameLineEdit.setText(str(TicketList.getInstance().getTicket(numberEntered)))
+
     def cancelEvent(self):
         """
         Nothing will be done if cancel button is hit
