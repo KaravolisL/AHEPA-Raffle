1080064da686962595c49b1e2ce3c3358d8bcce5
KaravolisL <lukejrkaravolis@gmail.com>
2019-11-07 22:34:20 -0500

    Additional work done on Edit Ticket Name

--- src/Windows/EditTicketWindow.py
+++ src/Windows/EditTicketWindow.py
@@ -6,6 +6,8 @@ from PyQt5.QtMultimediaWidgets import *
 
 from Windows.WindowBase import WindowBase
 from Tickets.TicketList import TicketList
+from Utils.Validators import validateTicketName, validateTicketNumber
+import Controller
 
 class EditTicketWindow(WindowBase):
     def __init__(self):
@@ -37,6 +39,7 @@ class EditTicketWindow(WindowBase):
         self.ticketNumberLineEdit = QLineEdit()
 
         self.currentNameLabel = QLabel('Current Name: ')
+        self.currentName = QLabel('')
 
         self.ticketNameLabel = QLabel('Ticket Name: ')
         self.ticketNameLabel.setAlignment(Qt.AlignCenter)
@@ -44,13 +47,14 @@ class EditTicketWindow(WindowBase):
         self.ticketNameLineEdit = QLineEdit()
 
         self.confirmButton = QPushButton('Change Name')
-        # self.confirmButton.clicked.connect()
+        self.confirmButton.clicked.connect(self.changeNameEvent)
         self.cancelButton = QPushButton('Cancel')
         self.cancelButton.clicked.connect(self.cancelEvent)
 
         self.layout.addWidget(self.ticketNumberLabel, 0, 0)
         self.layout.addWidget(self.ticketNumberLineEdit, 0, 1)
-        self.layout.addWidget(self.currentNameLabel, 1, 0, 2, 1)
+        self.layout.addWidget(self.currentNameLabel, 1, 0)
+        self.layout.addWidget(self.currentName, 1, 1)
         self.layout.addWidget(self.ticketNameLabel, 2, 0)
         self.layout.addWidget(self.ticketNameLineEdit, 2, 1)
         self.layout.addWidget(self.confirmButton, 3, 0)
@@ -60,13 +64,37 @@ class EditTicketWindow(WindowBase):
         """
         Initialize line edit events and validation
         """
+        self.numberEntered = 0
         self.ticketNumberLineEdit.textEdited.connect(self.ticketNumberEntered)
         self.ticketNumberLineEdit.setMaxLength(3)
-    
+
     def ticketNumberEntered(self):
-        numberEntered = int(self.ticketNumberLineEdit.text())
-        print(numberEntered)
-        self.ticketNameLineEdit.setText(str(TicketList.getInstance().getTicket(numberEntered)))
+        """
+        Validates the input then displays the ticket's current name
+        """
+        numberEnteredAsStr = self.ticketNumberLineEdit.text()
+        if validateTicketNumber(numberEnteredAsStr):
+            self.numberEntered = int(numberEnteredAsStr)
+            self.currentName.setText(str(TicketList.getInstance().getTicket(self.numberEntered)))
+        else:
+            self.numberEntered = 0
+            self.currentName.clear()
+
+    def changeNameEvent(self):
+        """
+
+        """
+        if self.numberEntered == 0:
+            return
+
+        newName = self.ticketNameLineEdit.text()
+        if validateTicketName(newName):
+            TicketList.getInstance().setTicketName(self.numberEntered, newName)
+            self.ticketNumberEntered()
+            Controller.notifyTicketNameChange(TicketList.getInstance().getTicket(self.numberEntered))
+        else:
+            # TODO: Is there anything to do here?
+            pass
 
     def cancelEvent(self):
         """
@@ -74,4 +102,3 @@ class EditTicketWindow(WindowBase):
         """
         self.close()
 
-        
