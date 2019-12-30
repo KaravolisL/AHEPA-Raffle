9b52f1d716ed65e338741893134c96abe2afecf1
Luke Karavolis <lukejrkaravolis@gmail.com>
2019-12-30 15:02:50 -0500

    Made new Raffle class

--- src/Raffle.py
+++ src/Raffle.py
@@ -1,20 +1,6 @@
-# PyQt libraries
-from PyQt5.QtWidgets import QApplication
-from PyQt5.QtGui import QIcon
 
-# System libraries
-import sys
-from datetime import datetime
 
-# Local libraries
-import Controller
 
-if __name__ == '__main__':
-    app = QApplication(sys.argv)
-    app.setApplicationName("AHEPA Raffle " + str(datetime.now().year))
-    app.setWindowIcon(QIcon(r'..\images\Icon.jpg'))
-    
-    # Initialize view and model
-    Controller.initialize()
-
-    app.exec_()
+class Raffle():
+    def __init__(self):
+        pass
