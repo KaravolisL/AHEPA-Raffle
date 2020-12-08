cc5c559d532b35b8cecf29bc2337687f1008e0be
Luke Karavolis <karavolisl@gmail.com>
2020-12-08 14:41:24 -0500

    Rework main

--- src/Main.py
+++ src/Main.py
@@ -1,17 +1,31 @@
-# PyQt libraries
-from PyQt5.QtWidgets import QApplication
-from PyQt5.QtGui import QIcon
+"""Main file to be run"""
 
-# System libraries
 import sys
 from datetime import datetime
+from argparse import ArgumentParser
 
-if __name__ == '__main__':
-    app = QApplication(sys.argv)
+from PyQt5 import QtWidgets
+
+EXIT_SUCCESS = 0
+
+def main():
+    """Main entry point of the application"""
+    argument_parser = ArgumentParser(
+        prog='python main.py'
+    )
+    argument_parser.add_argument('--no_gui', action='store_true',
+                                 help='Launches application without the user interface')
+    args = argument_parser.parse_args()
+
+    if args.no_gui:
+        return EXIT_SUCCESS
+
+    app = QtWidgets.QApplication(sys.argv)
     app.setApplicationName("AHEPA Raffle " + str(datetime.now().year))
 
-    # Create a Raffle instance
-    from Raffle import Raffle
-    raffle = Raffle()
-
     app.exec_()
+
+    return EXIT_SUCCESS
+
+if __name__ == '__main__':
+    sys.exit(main())
